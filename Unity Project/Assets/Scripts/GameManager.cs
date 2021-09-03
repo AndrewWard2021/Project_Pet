2e146711eb2f14a6dd59d72cd2ef29f88e12dd93
AndrewWard2021 <andrewward22499@gmail.com>
2021-09-03 12:35:02 -0500

    adds a time cycle to the game manager

--- Unity Project/Assets/Scripts/GameManager.cs
+++ Unity Project/Assets/Scripts/GameManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int weekCount;
+    private int yearCount;
     void Start()
     {
-        
+        weekCount = 0;
+        yearCount = 0;
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        //feed weekcount to display
+    }
+
+    public void NextWeek()
+    {
+        weekCount+=1;
+        if (weekCount=>52)
+        {
+            weekCount = weekCount%52;
+            yearCount+=1;
+        }
     }
 }
