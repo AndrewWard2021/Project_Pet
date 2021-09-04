fb3e55f87de786c9b223dfa65b2a63c57685eeaa
AndrewWard2021 <andrewward22499@gmail.com>
2021-09-04 12:58:41 -0500

    Display date in top right cornor and button will change the date

--- Unity Project/Assets/Scripts/GameManager.cs
+++ Unity Project/Assets/Scripts/GameManager.cs
@@ -1,28 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    #region Variables
     private int weekCount;
     private int yearCount;
+    public Text yearText;
+    public Text weekText;
+    #endregion
+    #region Singleton
+    public static GameManager instance;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More then one instance of game manager");
+        }
+        instance = this;
+    }
+    #endregion
+
     void Start()
     {
         weekCount = 0;
-        yearCount = 0;
+        yearCount = 1;
     }
     // Update is called once per frame
     void Update()
     {
         //feed weekcount to display
+        weekText.text = "Week: "+weekCount;
+        yearText.text = "Year: "+yearCount;
     }
 
     public void NextWeek()
     {
         weekCount+=1;
-        if (weekCount>=52)
+        if (weekCount>=53)
         {
-            weekCount = weekCount%52;
+            weekCount = weekCount%53;
             yearCount+=1;
         }
     }
